f8a34a4b3d0b4cf11afabb15e36f678602df40de
Eunji Kim <90446559+movmovmovmov@users.noreply.github.com>
2023-05-25 17:54:18 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        double a = (double) num1 / (double) num2 * 1000;
+
+        int answer = (int) a;
+        return answer;
+    }
+}
